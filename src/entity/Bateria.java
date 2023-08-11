7a5261aaac739cd30c71e274e8f2f78c8f039801
NahuelMBA <142049188+NahuelMBA@users.noreply.github.com>
2023-08-11 18:04:47 -0300

    Update Bateria.java

--- src/entity/Bateria.java
+++ src/entity/Bateria.java
@@ -10,18 +10,6 @@ package entity;
  */
 public class Bateria {
 
-    private double carga = 1000 ;
-
-    public Bateria(double carga) {
-        this.carga = carga;
-    }
-
-    public double getCarga() {
-        return carga;
-    }
-
-    public void setCarga(double carga) {
-        this.carga = carga;
-    }
+    
     
 }
