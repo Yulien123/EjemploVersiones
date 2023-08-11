91a2dd227842aabe5a745205c8ea7ceda38037cc
Bauti7 <juanbau2000@hotmail.es>
2023-08-11 18:31:39 -0300

    Julio, falta la clase robot!!

--- src/entity/Hombre.java
+++ src/entity/Hombre.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entity;
 
-/**
- *
- * @author julan
- */
+import java.util.Scanner;
+
 public class Hombre {
+    
+    public void JugarConRobot(Robot robot) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("Ingrese la acción (1-Avanzar, 2-Retroceder, 3-Bateria Actual , 4-Cargar , 5-Despertar , 6-Dormir ):");
+            int accion = scanner.nextInt();
+
+            switch (accion) {
+                case 1:
+                    System.out.println("Ingrese la cantidad de pasos:");
+                    int pasosAvanzar = scanner.nextInt();
+                    robot.Avanzar(pasosAvanzar);
+                    break;
+
+                case 2:
+                    System.out.println("Ingrese la cantidad de pasos:");
+                    int pasosRetroceder = scanner.nextInt();
+                    robot.retroceder(pasosRetroceder);
+                    break;
+
+                case 3:
+                    System.out.println("La beteria actual es: "+ robot.BateriaActual() );
+
+                    break;
+
+                case 4:
+                    System.out.println("Batería restante: " + robot.BateriaActual());
+                    break;
+
+                case 5:
+
+                    robot.Despierto();
+                    break;
+
+
+                case 6:
+
+                    robot.Dormir();
+                    break;
+
+                default:
+                    System.out.println("Acción inválida.");
+                    break;
+            }
+        }
+    }
     
 }
