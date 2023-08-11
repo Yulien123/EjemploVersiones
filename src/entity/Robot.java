ae4040cab115b3ee872e3cee06f54a0f55e3d171
Julio Flores <juliofloresth@gmail.com>
2023-08-11 18:40:39 -0300

    clase robot lista
    falta clase hombre

--- src/entity/Robot.java
+++ src/entity/Robot.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entity;
 
 /**
@@ -9,5 +6,70 @@ package entity;
  * @author julan
  */
 public class Robot {
-    
+
+    private int bateria;
+    private boolean dormido;
+
+    public Robot() {
+        bateria = 10000;
+        dormido = false;
+
+    }
+    public void Avanzar(int pasos) {
+        if (dormido == true) {
+            bateria -= pasos / 100 * 100;
+            System.out.println("Avanzando " + pasos + " pasos.");
+        } else {
+            System.out.println("No puedo avanzar, estoy dormido.");
+        }
+    }
+
+    public void retroceder(int pasos) {
+        if (dormido == true) {
+            bateria -= pasos / 100 * 100;
+            System.out.println("Retrocedio " + pasos + " pasos.");
+        } else {
+            System.out.println("No puedo retroceder mas , estoy dormido.");
+        }
+    }
+
+    public void Despierto() {
+
+        dormido = true;
+        System.out.println("El robot se desperto");
+
+    }
+
+    public void Dormir() {
+
+        dormido = false;
+
+        System.out.println("El robot se durmio");
+
+    }
+
+    public void Recargar() {
+
+        bateria = 1000;
+
+        System.out.println("La bateria se recargo");
+    }
+
+    public boolean Bateriallena() {
+
+        return bateria == 1000;
+
+    }
+
+    public boolean BateriaVacia() {
+
+        return bateria == 0;
+
+    }
+
+    public int BateriaActual() {
+
+        return bateria;
+
+    }
 }
