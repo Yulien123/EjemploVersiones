ea6892171a4dd3b5c05731fc4585ab91d7640325
NahuelMBA <namercado28@gmail.com>
2023-08-11 18:24:16 -0300

    Actualizado-Bateria

--- src/entity/Bateria.java
+++ src/entity/Bateria.java
@@ -1,15 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entity;
 
-/**
- *
- * @author julan
- */
+
 public class Bateria {
 
-    
+    private double carga = 1000 ;
+
+    public Bateria(double carga) {
+        this.carga = carga;
+    }
+
+    public double getCarga() {
+        return carga;
+    }
+
+    public void setCarga(double carga) {
+        this.carga = carga;
+    }
     
 }
